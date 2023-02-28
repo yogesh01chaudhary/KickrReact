64a8f7459db42ce251a5e46435993d4a61dd1e36
USIT <dikshalalit141@gmail.com>
2023-02-28 11:28:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+//--------------------------------React Hooks--------------------------------------//
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+
+
+//--------------------------------Pages--------------------------------------//
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Navbar.css"
+import Logo from "../../Assests/KickrWhiteLogo.png"
+
+export default function Navbar() {
+  return (
+    <>
+        <div className='navbarBg'>
+            <div className='navbarBox'>
+                hello
+            </div>
+        </div>
+    </>
+  )
+}
